0e5c9d6aa9ada9df66a9755467f7cfa06ae7c779
Grzegorz Błaszczyk <grzegorz.blaszczyk@goodylabs.com>
2016-11-01 20:16:07 +0100

    ADD: function verify_if_installed_with_dot for Mac and Ubuntu

--- bash/mac/verify_if_installed_with_dot.inc.sh
+++ bash/mac/verify_if_installed_with_dot.inc.sh
@@ -16,7 +16,7 @@ function verify_if_installed_with_dot() {
   if [ "x${1}" == "x" ] || [ "x${2}" == "x" ] || [ ! -f ${2} ]; then
     echo ""
     echo "Please install $1 on your system. Probably you can do it by running:"
-    echo "sudo apt-get install ${1}"
+    echo "brew install ${1}"
     echo ""
     exit 1;
   else
