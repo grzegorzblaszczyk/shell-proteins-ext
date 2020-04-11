f491cad6a8a092feda11cb54ef70d4b0c8f7eb12
Grzegorz Błaszczyk <grzegorz.blaszczyk@goodylabs.com>
2020-04-11 23:48:00 +0200

    ADD: convert/jpg2heic module based on other modules + example script with usage of convert/jpg2heic function

---
+++ bash/convert/jpg2heic.inc.sh
@@ -0,0 +1,29 @@
+# Function: jpg2heic
+#
+# Requirements:
+# * ImageMagick
+#
+# input params:
+#   input JPG file
+#   output HEIC file
+# output:
+#
+# error:
+#
+
+function jpg2heic {
+  if [ "x${1}" == "x" ] || [ "x${2}" == "x" ] || [ ! -f ${1} ]; then
+    echo "Please provide a valid input file!"
+    echo ""
+    exit 1;
+  else
+    CONVERT=`which convert`
+    RM=`which rm`
+    INPUT_FILE="$1"
+    OUTPUT_FILE="$2"
+
+    echo "Converting ${INPUT_FILE} to ${OUTPUT_FILE} ..."
+
+    ${CONVERT} "${INPUT_FILE}" "${OUTPUT_FILE}"
+  fi
+}
---
+++ examples/convert_jpg_file_to_heic_file.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# This script:
+# * converts JPG file to HEIC file (iPhone image)
+
+source "$HOME/bin/shell-proteins/bash/base.inc.sh"
+protein_require "core/print_usage"
+protein_require "grzegorzblaszczyk/shell-proteins-ext/bash/os_agnostic/verify_if_installed_with_dot" "https://raw.githubusercontent.com/grzegorzblaszczyk/shell-proteins-ext/master/bash/os_agnostic/verify_if_installed_with_dot.inc.sh"
+protein_require "grzegorzblaszczyk/shell-proteins-ext/bash/convert/jpg2heic" "https://raw.githubusercontent.com/grzegorzblaszczyk/shell-proteins-ext/master/bash/convert/jpg2heic.inc.sh"
+
+if [ "x${1}" == "x" ] || [ "x${2}" == "x" ]; then
+  print_usage "HEIC input file" "PDF output file"
+  exit 1
+fi
+
+verify_if_installed_with_dot "convert" "/usr/local/bin/convert"
+echo ""
+
+jpg2heic ${1} ${2}
+
